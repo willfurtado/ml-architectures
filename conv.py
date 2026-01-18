551fa29a48424eca6f782ed6a9334bfd3899cc28
William Furtado <willfurtado@berkeley.edu>
2026-01-18 13:28:09 -0500

    Add MaxPool2d layer

--- conv.py
+++ conv.py
@@ -92,6 +92,52 @@ class Conv2d:
         return f"{self.__class__.__name__}(in_channels={self.in_channels}, out_channels={self.out_channels})"
 
 
+class MaxPool2d:
+    """
+    Max pooling layer
+    """
+
+    def __init__(self, kernel_size: tuple[int, int], stride: int = 1):
+        """
+        Creates an instance of the `MaxPool2d` class
+        """
+        self.kernel_size = kernel_size
+        self.stride = stride
+
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        """
+        Forward pass for the max pooling layer
+
+        Parameters:
+            x (np.ndarray): Input batch of shape (B, C, H, W)
+
+        Returns:
+            np.ndarray: Downsampled batch of shape (B, C, H', W')
+        """
+        B, C, H, W = x.shape
+        K_h, K_w = self.kernel_size
+
+        H_prime = int((H - K_h) / self.stride) + 1
+        W_prime = int((W - K_w) / self.stride) + 1
+
+        out = np.empty(shape=(B, C, H_prime, W_prime))
+
+        for b in range(B):
+            for c in range(C):
+                for h in range(H_prime):
+                    for w in range(W_prime):
+                        h_start, w_start = h * self.stride, w * self.stride
+                        input_slice = x[
+                            b, c, h_start : h_start + K_h, w_start : w_start + K_w
+                        ]  # Of shape (K_h, K_w)
+
+                        out[b, c, h, w] = np.max(input_slice)
+        return out
+
+    def __call__(self, x: np.ndarray) -> np.ndarray:
+        return self.forward(x=x)
+
+
 if __name__ == "__main__":
     layer = Conv2d(
         in_channels=3, out_channels=8, kernel_size=(3, 3), stride=2, padding=1
